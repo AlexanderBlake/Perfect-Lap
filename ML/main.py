65c7ea9c05694143a9dc0c125f45f17e00c9e0fb
Alexander Blake <alex.blake24@gmail.com>
2024-05-25 09:47:23 -0700

    Pylint and Model Optimization

--- ML/main.py
+++ ML/main.py
@@ -1,123 +1,207 @@
+'''
+This module conducts machine learning experiments to find the best model and
+the most important features.
+'''
+
+from math import sqrt
+
 import numpy as np
 import pandas as pd
-from math import sqrt
-from sklearn import tree
 import matplotlib.pyplot as plt
 from prettytable import PrettyTable
+
+from sklearn import tree
 from sklearn.svm import LinearSVR, NuSVR, SVR
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.neural_network import MLPRegressor
-from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import cross_val_score, KFold, train_test_split
 
-def main():
-    df = pd.read_csv('VSK - Sheet1.csv')
+RANDOM = 42
+SCORE = 'neg_root_mean_squared_error'
 
-    df = df.groupby(['Race 1 DateTime'])
+def preprocess_data(data):
+    '''
+    This function performs some operation.
+
+    Parameters:
+    data (int): Description of param1.
+
+    Returns:
+    bool: Description of the return value.
+    '''
+    data = data.groupby(['Race 1 DateTime'])
     scaler = MinMaxScaler()
-    
+
     scaled_groups = []
-    for name, group in df:
-        group['Race 1 Fastest Time'] = scaler.fit_transform(group['Race 1 Fastest Time'].values.reshape(-1, 1))
+    for _, group in data:
+        group['Race 1 Fastest Time'] = scaler.fit_transform(
+            group['Race 1 Fastest Time'].values.reshape(-1, 1))
+        group['Race 1 Gap to Leader'] = scaler.fit_transform(
+            group['Race 1 Gap to Leader'].values.reshape(-1, 1))
         scaled_groups.append(group)
 
-    df = pd.concat(scaled_groups)
+    data = pd.concat(scaled_groups)
 
     for col in ['Race 1 Kart Num', 'Final Kart Num', 'Driver Name']:
-        one_hot_encoded = pd.get_dummies(df[col], prefix=col)
-        df = pd.concat([df, one_hot_encoded], axis=1)
+        one_hot_encoded = pd.get_dummies(data[col], prefix=col)
+        data = pd.concat([data, one_hot_encoded], axis=1)
 
-    X = df[['Final Starting Pos', 'Race 1 Fastest Time'] +
-           list(df.filter(regex='Track|Final Kart Num_|Driver Name_').columns)]
-    y = df['Final Finish Pos']
+    x_data = data[['Final Starting Pos', 'Race 1 Fastest Time', 'Race 1 Gap to Leader'] +
+                  list(data.filter(regex='Driver Name_').columns)]
+    y_data = data['Final Finish Pos']
 
+    return x_data, y_data
+
+def conduct_experiments(x_data, y_data) -> list:
+    '''
+    This function performs some operation.
+
+    Parameters:
+    x_data (int): Description of param1.
+    y_data (str): Description of param2.
+
+    Returns:
+    list: Description of the return value.
+    '''
     results = []
 
-    models = [(LinearRegression(), 'Linear Regression'),
-              (KNeighborsRegressor(), 'KNN'),
-              (RandomForestRegressor(random_state=42), 'Random Forest'),
-              (LinearSVR(random_state=42, dual='auto', max_iter=4000), 'Linear SVM')]
+    models = [(KNeighborsRegressor(), 'KNN'),
+              (LinearSVR(random_state=RANDOM, dual='auto', max_iter=8000), 'Linear SVM')]
     for model, name in models:
-        results.append((-1 * cross_val_score(model, X, y, scoring='neg_root_mean_squared_error').mean(), name))
+        results.append((-1 * cross_val_score(model, x_data, y_data, scoring=SCORE).mean(), name))
 
-    for depth in range(1, 17):
-        results.append((-1 * cross_val_score(tree.DecisionTreeRegressor(random_state=42, max_depth=depth), X, y, scoring='neg_root_mean_squared_error').mean(), 'Decision Tree Max Depth: ' + str(depth)))
-    
+    for depth in range(1, 9):
+        results.append((-1 * cross_val_score(
+            RandomForestRegressor(random_state=RANDOM, max_depth=depth), x_data, y_data,
+            scoring=SCORE).mean(), 'Random Forest Max Depth: ' + str(depth)))
+
     for kern in ['rbf', 'linear', 'poly', 'sigmoid']:
-        results.append((-1 * cross_val_score(NuSVR(kernel=kern), X, y, scoring='neg_root_mean_squared_error').mean(), 'NU SVM Kernel: ' + kern))
-        results.append((-1 * cross_val_score(SVR(kernel=kern), X, y, scoring='neg_root_mean_squared_error').mean(), 'SVM Kernel: ' + kern))
+        results.append((-1 * cross_val_score(NuSVR(kernel=kern), x_data, y_data,
+                                             scoring=SCORE).mean(), 'NU SVM Kernel: ' + kern))
+        results.append((-1 * cross_val_score(SVR(kernel=kern), x_data, y_data,
+                                             scoring=SCORE).mean(), 'SVM Kernel: ' + kern))
 
-    kf = KFold()
-    for guesses in range(10, 15):
-        temp = 0
-        for i, (train_index, test_index) in enumerate(kf.split(X)):
-            temp += mean_squared_error(np.random.randint(1, guesses, size=len(X.iloc[test_index])), y.iloc[test_index])
-        results.append((temp / 5, 'Random Guessing 1 - ' + str(guesses)))
+    k_fold = KFold()
+    temp = 0
+    for _, (_, test_index) in enumerate(k_fold.split(x_data)):
+        temp += sqrt(mean_squared_error(np.full(len(test_index), 6), y_data.iloc[test_index]))
+    results.append((temp / 5, 'BASELINE: Median Guessing'))
 
     '''
     for ac in ['relu', 'identity', 'logistic', 'tanh']:
         for so in ['lbfgs', 'sgd', 'adam']:
             for lr in ['constant', 'invscaling', 'adaptive']:
                 if so == 'sgd':
-                    results.append((-1 * cross_val_score(MLPRegressor(random_state=42, max_iter=12800, activation=ac, solver=so, learning_rate=lr), X, y, scoring='neg_root_mean_squared_error').mean(), 'Neural Network ' + ac + ' ' + so + ' ' + lr))
+                    results.append((-1 * cross_val_score(
+                        MLPRegressor(random_state=RANDOM, max_iter=6400, activation=ac,
+                                     solver=so, learning_rate=lr), x_data, y_data,
+                                     scoring='neg_root_mean_squared_error').mean(),
+                                     'Neural Network ' + ac + ' ' + so + ' ' + lr))
                 else:
-                    results.append((-1 * cross_val_score(MLPRegressor(random_state=42, max_iter=12800, activation=ac, solver=so), X, y, scoring='neg_root_mean_squared_error').mean(), 'Neural Network ' + ac + ' ' + so + ' ' + lr))
+                    results.append((-1 * cross_val_score(
+                        MLPRegressor(random_state=RANDOM, max_iter=6400, activation=ac,
+                                     solver=so), x_data, y_data,
+                                     scoring='neg_root_mean_squared_error').mean(),
+                                     'Neural Network ' + ac + ' ' + so + ' ' + lr))
                     break
     '''
 
     results.sort()
-    pt_table = PrettyTable(['Rank', 'Model Description', 'Root Mean Square Error (RMSE)'])
+    return results
 
-    for i in range(len(results)):
-        score, model = results[i]
-        pt_table.add_row([i + 1, model, score])
+def find_importances(x_data, regr) -> list:
+    '''
+    This function performs some operation.
 
-    print(pt_table)
-    print()
+    Parameters:
+    x_data (int): Description of param1.
+    regr (str): Description of param2.
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
-    regr = tree.DecisionTreeRegressor(random_state=42, max_depth=2).fit(X_train, y_train)
-    
-    y_pred = regr.predict(X_test)
-
-    X_test['predicted'] = y_pred
-    X_test['actual'] = y_test
-    X_test['error'] = y_test - y_pred
-    pd.set_option('display.max_columns', None)
-    #print(X_test)
-
-    plt.figure(figsize=(100, 100))
-    tree.plot_tree(regr, feature_names=X.columns, filled=True)
-    plt.savefig('decision_tree_plot.png')
+    Returns:
+    list: Description of the return value.
+    '''
 
     results = []
     importances = {'Driver Name': 0, 'Final Kart Num': 0, 'Track': 0}
-    for i in range(len(X.columns)):
-        if 'Driver Name' in X.columns[i]:
+
+    for i, col in enumerate(x_data.columns):
+        if 'Driver Name' in col:
             importances['Driver Name'] += regr.feature_importances_[i]
-        elif 'Final Kart Num' in X.columns[i]:
+        elif 'Final Kart Num' in col:
             importances['Final Kart Num'] += regr.feature_importances_[i]
-        elif 'Track' in X.columns[i]:
+        elif 'Track' in col:
             importances['Track'] += regr.feature_importances_[i]
         else:
-            results.append((regr.feature_importances_[i], X.columns[i]))
+            results.append((regr.feature_importances_[i], col))
 
     for key, value in importances.items():
         results.append((value, key))
 
     results.sort(reverse=True)
-    pt_table = PrettyTable(['Rank', 'Feature', 'Importances'])
+    return results
 
-    for i in range(len(results)):
-        importance, feature = results[i]
-        pt_table.add_row([i + 1, feature, importance])
+def display_table(header: list[str], results: list) -> None:
+    '''
+    This function prints a pretty table.
+
+    Parameters:
+    header (list[str]): Description of param1.
+    results (list): Description of param2.
+
+    Returns:
+    None
+    '''
+    pt_table = PrettyTable(header)
+
+    for i, result in enumerate(results):
+        value, name = result
+        if 'BASELINE' in name:
+            pt_table.add_row(['','',''])
+        pt_table.add_row([i + 1, name, value])
+        if 'BASELINE' in name:
+            pt_table.add_row(['','',''])
 
     print(pt_table)
     print()
 
-    print(len(X.columns))
+def main() -> None:
+    '''
+    This function runs the program.
+
+    Parameters:
+    None
+
+    Returns:
+    None
+    '''
+
+    data = pd.read_csv('VSK - Sheet1.csv')
+    x_data, y_data = preprocess_data(data)
+    results = conduct_experiments(x_data, y_data)
+
+    display_table(['Rank', 'Model Description', 'Root Mean Square Error (RMSE)'], results)
+
+    x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, random_state=RANDOM)
+    regr = RandomForestRegressor(random_state=RANDOM, max_depth=2).fit(x_train, y_train)
+
+    y_pred = regr.predict(x_test)
+
+    x_test['predicted'] = y_pred
+    x_test['actual'] = y_test
+    x_test['error'] = y_test - y_pred
+    pd.set_option('display.max_columns', None)
+    #print(X_test)
+
+    plt.figure(figsize=(100, 100))
+    tree.plot_tree(regr.estimators_[0], feature_names=x_data.columns, filled=True)
+    plt.savefig('decision_tree_plot.png')
+
+    results = find_importances(x_data, regr)
+    display_table(['Rank', 'Feature', 'Importances'], results)
+
+    print(len(x_data.columns))
 
 main()
